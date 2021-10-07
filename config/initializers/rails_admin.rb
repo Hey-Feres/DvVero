ca04243004c2363b7dd9da0b859f1617c9e01353
Hey-Feres <bruno@feres.dev>
2021-10-07 17:49:04 -0300

    Update

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -58,10 +58,10 @@ RailsAdmin.config do |config|
       field :title
       field :content
       field :sort_position
-      field :background_color, :color
-      field :text_color, :color
       field :first_image
       field :second_image
+      field :background_color, :color
+      field :text_color, :color
       field :active
     end
   end
